207adbdda846cef1d0f0f5f3bb85e9c0956b0ed4
Tomas Saghy <tomas.saghy@riesenia.com>
2024-06-04 10:51:38 +0200

    Transfer to riesenia

--- Formula/pass.rb
+++ Formula/pass.rb
@@ -1,7 +1,7 @@
 class Pass < Formula
     desc "Simplified Passbolt CLI access"
-    homepage "https://github.com/rshop/homebrew-pass"
-    url "https://github.com/rshop/homebrew-pass/archive/refs/tags/v1.0.1.tar.gz"
+    homepage "https://github.com/riesenia/homebrew-pass"
+    url "https://github.com/riesenia/homebrew-pass/archive/refs/tags/v1.0.1.tar.gz"
     sha256 "87fa74a3e1aefbd21ab79c72bb2b93dd047ca2a999e41764c7cded8431085128"
     license "MIT"
 
