2ac6282b23f751bc564a9a262b0747c29d40e222
Tomas Saghy <tomas.saghy@gmail.com>
2024-08-28 15:36:36 +0200

    switch php wip

---
+++ Formula/switch-php.rb
@@ -0,0 +1,10 @@
+class SwitchPhp < Formula
+    desc "Installs multiple PHP versions and switches between them"
+    homepage "https://github.com/riesenia/homebrew-switch-php"
+    url ""
+    sha256 ""
+    license "MIT"
+
+    depends_on "riesenia/utils/php@7.4"
+    depends_on "shivammathur/extensions/imap@7.4"
+    depends_on "shivammathur/extensions/xdebug@7.4"
