1df8ba69805e850af56fec6a5b7ccbb11aa42c4d
Tomas Saghy <tomas.saghy@gmail.com>
2025-04-08 14:49:48 +0200

    update switch-php

--- Formula/switch-php.rb
+++ Formula/switch-php.rb
@@ -1,8 +1,8 @@
 class SwitchPhp < Formula
     desc "Installs multiple PHP versions and switches between them"
     homepage "https://github.com/riesenia/homebrew-switch-php"
-    url "https://github.com/riesenia/homebrew-switch-php/archive/refs/tags/v0.0.1.tar.gz"
-    sha256 "d114107ba0e3f044dbf6b14811a5d6f99c639447071ab6fac47d644be416f087"
+    url "https://github.com/riesenia/homebrew-switch-php/archive/refs/tags/v0.0.2.tar.gz"
+    sha256 "cb9a231825b9d7b91a40dee62d34f5ac46e31c822ceffd9a95e7e14de9c41690"
     license "MIT"
 
     depends_on "httpd"
@@ -18,6 +18,10 @@ class SwitchPhp < Formula
     depends_on "shivammathur/php/php@8.3"
     depends_on "shivammathur/extensions/imap@8.3"
     depends_on "shivammathur/extensions/xdebug@8.3"
+
+    depends_on "shivammathur/php/php@8.4"
+    depends_on "shivammathur/extensions/imap@8.4"
+    depends_on "shivammathur/extensions/xdebug@8.4"
 
     def install
         bin.install "switch-php"
